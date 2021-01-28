8afacfef605e597dfffe021c866044d6f79a89f7
Diego Pereira <diego.pereira@farfetch.com>
2021-01-28 22:10:19 +0000

    chore: Refactor catalog project

--- src/Catalog/Catalog.Support/Configurations/CatalogConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Catalog.Support.Configurations
-{
-    public class CatalogConfiguration
-    {
-        public AuthConfiguration Authentication { get; set; }
-        public MongoDbConfiguration MongoDb { get; set; }
-    }
-}
